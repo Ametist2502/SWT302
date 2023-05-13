55f83ca0b95845f892252c109fd99b5fcf30bc41
Ametist2502 <whaleblue2002@gmail.com>
2023-05-13 16:12:26 +0700

    update code SWT302 Lab1

---
+++ exe5.py
@@ -0,0 +1,20 @@
+def calculate_price_Tdque(baseprice, specialprice, extraprice, extras, discount):
+     addon_discount = 0
+     result = 0
+     if extras >= 3:
+          addon_discount = 10
+     elif extras >= 5:
+          addon_discount = 15
+     if discount > addon_discount:
+          addon_discount = discount
+     result = baseprice/100.0*(100-discount) + specialprice + \
+          extraprice/100.0*(100-addon_discount)
+     return result
+
+baseprice = float(input("Enter baseprice: "))
+specialprice = float(input("Enter specialprice: "))
+extraprice = float(input("Enter extraprice: "))
+extras = int(input("Enter extras: "))
+discount = float(input("Enter discount: "))
+
+print(calculate_price_Tdque(baseprice, specialprice, extraprice, extras, discount))
